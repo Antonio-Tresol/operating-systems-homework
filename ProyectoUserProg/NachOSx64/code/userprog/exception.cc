909bdc096e30377c028972bbd7b79eea1ad0f06c
Antonio Badilla-Olivas <anthonny.badilla@ucr.ac.cr>
2023-05-23 20:56:04 -0600

    adding fork and commenting other syscall better, also fixing returns on void syscalls

--- ProyectoUserProg/NachOSx64/code/userprog/exception.cc
+++ ProyectoUserProg/NachOSx64/code/userprog/exception.cc
@@ -30,17 +30,68 @@
 #include "syscall.h"
 #include "system.h"
 
+/**
+ * @brief The entry point for a new user thread in NachOS.
+ *
+ * This function initializes the new user thread's registers, restores the
+ * thread's state from its address space, and sets the thread's program
+ * counter to the start of the user function passed in as a parameter. The
+ * machine then starts executing the new user thread. This function does not
+ * return, and it is an error if it does.
+ *
+ * @param p A pointer to the start of the user function that the new thread
+ * will execute.
+ */
+void NachosForkThread(void* p) {
+  // Initialize the current thread's registers. This involves resetting
+  // the context to a known state, ready for a new user thread to start
+  // executing.
+  currentThread->space->InitRegisters();
+  // Restores the current thread's state from its address space.
+  // This involves copying the current state of the thread (including the values
+  // of its registers and the contents of its stack) into the current address
+  // space.
+  currentThread->space->RestoreState();
+  // Set up the return address register with a dummy value (4)
+  machine->WriteRegister(RetAddrReg, 4);
+  // Set the program counter to the start of the function to execute. This is
+  // passed in as the argument 'p'. 'PCReg' is the program counter register.
+  machine->WriteRegister(PCReg, reinterpret_cast<long>(p));
+
+  // Set the next program counter (NextPCReg). This will be 'PCReg + 4', because
+  // we increment PCReg after every instruction.
+  machine->WriteRegister(NextPCReg, reinterpret_cast<long>(p) + 4);
+  // Start executing the new user thread.
+  machine->Run();
+  // We should never get past this point because a new thread has taken over
+  // execution. If we do reach this point, there is an error.
+  ASSERT(false);
+}
+
+/**
+ * @brief Increments the program counters in NachOS.
+ *
+ * This function is called after a system call in NachOS. It increments the
+ * values of the program counters PrevPCReg, PCReg, and NextPCReg by 4. This is
+ * necessary to prepare for the next instruction to be executed.
+ */
 void NachOS_IncreasePC() {
-  machine->WriteRegister(PrevPCReg, PCReg);
+  // Write the value of PCReg into PrevPCReg, moving the previous program
+  // counter forward to the current program counter.
+  machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+  // Write the value of NextPCReg into PCReg, moving the current program counter
+  // forward to the next program counter.
   machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+  // Increment the value of NextPCReg by 4 to prepare for the next instruction.
+  // This keeps our program counters moving forward through the instructions.
   machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg) + 4);
-}  // returnFromSystemCall
-/*
+}
+/**
  *  System call interface: Halt()
  */
 void NachOS_Halt() {  // System call 0
 
-  DEBUG('a', "Shutdown, initiated by user program.\n");
+  DEBUG('h', "Shutdown, initiated by user program.\n");
   interrupt->Halt();
 }
 
@@ -53,8 +104,8 @@ void NachOS_Exit() {  // System call 1
   // Yield control to the next thread.
   currentThread->Yield();
   // Print exit status
-  printf("Thread %s exited with status %d\n", currentThread->getName(),
-         exitStatus);
+  DEBUG('a', "Thread %s exited with status %d\n", currentThread->getName(),
+        exitStatus);
   // Finish the current thread's execution.
   currentThread->Finish();
   // Advance program counter.
@@ -72,189 +123,212 @@ void NachOS_Exec() {  // System call 2
 void NachOS_Join() {  // System call 3
 }
 
-/*
- *  System call interface: void Create( char * )
+/**
+ * @brief NachOS Create System call
+ * This system call is used to create a new file with read and write permissions
+ * for the user. The file name is read from user memory, as specified by the
+ * contents of register 4.
+ *
+ * System call interface: void Create(char *)
  */
 void NachOS_Create() {  // System call 4
   // Read the file name from user memory, as indicated by register 4.
   int64_t fileNameAddress = machine->ReadRegister(4);
-  char fileName[FILENAME_MAX + 1];  // buffer for the file name
+  char fileName[FILENAME_MAX + 1];  // Buffer for the file name.
   int fileNameChar;  // Character buffer to read the file name one character at
                      // a time.
-  unsigned int fileNameLen = 0;  // Length of the file name
+  u_int32_t fileNameLen = 0;  // Length of the file name.
   // Read the file name from user memory.
   do {
     machine->ReadMem(fileNameAddress + fileNameLen, 1, &fileNameChar);
     fileName[fileNameLen++] = fileNameChar;
   } while (fileNameChar != 0 && fileNameLen < FILENAME_MAX);
-  fileName[fileNameLen] = '\0';  // Null-terminate the C-string.
+  // Null-terminate the C-string.
+  fileName[fileNameLen] = '\0';
+  // Check if the file name length is valid.
   if (fileNameLen <= FILENAME_MAX) {
-    // Create the file with the specified name.
-    // read and write permissions for the user
+    // Create the file with the specified name and read-write permissions for
+    // the user.
     int status = creat(fileName, S_IRUSR | S_IWUSR);
+    // Check if the file was successfully created.
     if (status == -1) {
-      printf("Unable to create the file: %s\n", fileName);
-      // Set return value to -1 to signify error
-      machine->WriteRegister(2, -1);
+      DEBUG('k', "Unable to create file: %s\n", fileName);
     } else {
-      printf("Created file: %s\n", fileName);
+      DEBUG('k', "Created file: %s\n", fileName);
       OpenFileId openFileId = static_cast<OpenFileId>(status);
+      // Open the newly created file.
       openFileId = currentThread->openFiles->Open(openFileId);
-      // Set return value to 0 to signify success
-      machine->WriteRegister(2, 0);
     }
   } else {
-    printf("File name too long: %s\n", fileName);
-    // Set return value to -1 to signify error
-    machine->WriteRegister(2, -1);
+    // If the file name is too long, report an error.
+    DEBUG('k', "File name too long: %s\n", fileName);
   }
-  // Advance program counter
+  // Increment the program counter.
   NachOS_IncreasePC();
 }
 
-/*
- *  System call interface: OpenFileId Open( char * )
+/**
+ * @brief NachOS Open System call
+ * This system call is used to open a file, returning a file descriptor on
+ * success.
+ * System call interface: OpenFileId Open( char * name)
  */
 void NachOS_Open() {  // System call 5
+  // Read file name address from register 4
   int64_t fileNameAddress = machine->ReadRegister(4);
-
+  // Buffer to store each character from the file name
   int32_t nameCharBuffer = 0;
-  std::string fileName;  // Create string object
-
+  // Initialize string object to hold the file name
+  std::string fileName;
   do {
+    // Read each character of the file name from memory
     machine->ReadMem(fileNameAddress + fileName.size(), 1, &nameCharBuffer);
     if (nameCharBuffer != 0) {
-      // Add character to string
+      // If the character is not the end of string, add it to the file name
       fileName.push_back(static_cast<char>(nameCharBuffer));
     }
-  } while (nameCharBuffer != 0);  // ends on end of file character
+    // Loop continues until it encounters end of file character
+  } while (nameCharBuffer != 0);
+  // Open the file in read-write and append mode
   int fd = open(fileName.c_str(), O_RDWR | O_APPEND);
+  // If the file couldn't be opened, write -1 to register 2 and increment the PC
   if (fd == -1) {
     machine->WriteRegister(2, -1);
     NachOS_IncreasePC();
     return;
   }
+  // Cast the file descriptor to an OpenFileId
   OpenFileId openFileId = static_cast<OpenFileId>(fd);
-
+  // Update the open file table for the current thread
   openFileId = currentThread->openFiles->Open(openFileId);
-
+  // Write the file descriptor to register 2
   machine->WriteRegister(2, openFileId);
-
+  // Increment the program counter
   NachOS_IncreasePC();
 }
 
 /**
- *  System call interface: OpenFileId Write( char *, int, OpenFileId )
+ * @brief The Write system call for NachOS.
+ *
+ * This system call writes from a buffer in memory to an open file, or the
+ * console. The number of characters written is returned in register 2. If the
+ * file descriptor is invalid, or an error occurs, -1 is returned.
+ * System call interface: void Write(char *buffer, int size, OpenFileId id)
  */
 void NachOS_Write() {  // System call 7
   // Read parameters from registers
-  int inputBuffer = machine->ReadRegister(4);
-  int bufferSize = machine->ReadRegister(5);
+  // Read the address of the buffer from register 4
+  int32_t inputBuffer = machine->ReadRegister(4);
+  // Read the size from register 5
+  int32_t bufferSize = machine->ReadRegister(5);
+  // Read the file descriptor from register 6
   OpenFileId fileDescriptor = machine->ReadRegister(6);
-
-  // Print the NachOS handle
-  DEBUG('a', "Writing to file %d (NachOS handle)...\n", fileDescriptor);
-
-  // Create a buffer of size bufferSize
+  // TODO: Consider using a semaphore here if multiple threads are trying to
+  // write to the same file.
+  // Debug message about the NachOS file handle we're writing to
+  DEBUG('w', "Writing to file %d (NachOS handle)...\n", fileDescriptor);
+  // Create a local buffer of size bufferSize
   char buffer[bufferSize];
   int translation;
-
   // Read bufferSize number of bytes from the input buffer into our local buffer
-  for (int i = 0; i < bufferSize; i++) {
+  for (int32_t i = 0; i < bufferSize; i++) {
     machine->ReadMem(inputBuffer + i, 1, &translation);
     buffer[i] = translation;
   }
-
-  // Check file descriptor
+  // Check the file descriptor to determine where to write data
   switch (fileDescriptor) {
     case ConsoleInput:
-      // Writing to console input is not allowed
-      machine->WriteRegister(2, -1);
+      // Writing to the console input is not allowed, report error (-1)
+      DEBUG('w', "Writing to console input is not allowed!\n");
       break;
-
     case ConsoleOutput:
+      DEBUG('w', "Writing to console output...\n");
+      // Write the buffer to the console output
       write(1, buffer, bufferSize);
       write(1, "\n", 1);
-      // Write the number of bytes written to the output buffer
-      machine->WriteRegister(2, bufferSize);
       break;
-
     default:
-      // Writing to file
-      DEBUG('a', "Writing to file %d (NachOS handle) ...\n", fileDescriptor);
+      // We're writing to a file, print debug message
+      DEBUG('w', "Writing to file %d (NachOS handle)...\n", fileDescriptor);
       // Check if the file is open
       bool fileIsOpen = currentThread->openFiles->isOpened(fileDescriptor);
       if (fileIsOpen) {
-        // Get the UNIX handle
-        int unixFileHandle =
+        // Get the UNIX file handle
+        int32_t unixFileHandle =
             currentThread->openFiles->getUnixHandle(fileDescriptor);
-        // Write to the UNIX file
+        // Write the buffer to the UNIX file
         write(unixFileHandle, buffer, bufferSize);
-        // Write the number of bytes written to the register
+        // Write the number of characters written to register 2
         machine->WriteRegister(2, bufferSize);
-        // Print the success message
-        DEBUG('a',
-              "Successfully wrote %d bytes to file %d (UNIX) | %d "
-              "(NachOS).\n",
+        // Debug message for successful file write
+        DEBUG('w',
+              "Successfully wrote %d bytes to file %d (UNIX) | %d (NachOS).\n",
               bufferSize, unixFileHandle, fileDescriptor);
       } else {
-        // If the file is not open, write -1 to the register
-        machine->WriteRegister(2, -1);
+        // If the file is not open, report error (-1)
+        DEBUG('w', "File %d (NachOS handle) is not open!\n", fileDescriptor);
       }
       break;
   }
-  // Update the Program Counter registers
+  // Increment the program counter
+  // TODO: signal semaphore here if multiple threads are trying to write to the
   NachOS_IncreasePC();
 }
 
-/*
- *  System call interface: OpenFileId Read( char *, int, OpenFileId )
+/**
+ * @brief The Read system call for NachOS.
+ *
+ * This system call reads from an open file, or the console, into a buffer in
+ * memory. The number of characters read is returned in register 2. If the file
+ * descriptor is invalid, or an error occurs, -1 is returned.
+ * System call interface int Read(buffer, size, id)
  */
 void NachOS_Read() {  // System call 6
   // Obtain the parameters from the machine registers
-  int bufferAddr = machine->ReadRegister(4);
-  int size = machine->ReadRegister(5);
+  // Read the address of the buffer from register 4
+  int32_t bufferAddr = machine->ReadRegister(4);
+  // Read the size from register 5
+  int32_t size = machine->ReadRegister(5);
+  // Read the file descriptor from register 6
   OpenFileId descriptorFile = machine->ReadRegister(6);
-
   // Allocate a buffer to store the data that's read
   char* readBuffer = new char[size + 1];
-
   switch (descriptorFile) {
     case ConsoleInput: {
-      // Read from console
+      // Read from the console into the buffer
       scanf("%s", readBuffer);
-
-      int readChar = 0;
+      int32_t readChar = 0;
       // As long as we haven't reached the size or end of string
-      while (readChar < size && readChar < (int)strnlen(readBuffer, size) &&
+      while (readChar < size &&
+             readChar < static_cast<int32_t>(strnlen(readBuffer, size)) &&
              readBuffer[readChar] != 0) {
-        // Write character into memory
+        // Write each character from the buffer into memory at the corresponding
+        // address
         machine->WriteMem(bufferAddr + readChar, 1, readBuffer[readChar]);
         readChar++;
       }
-
-      // Write number of characters read to register 2
+      // Write the number of characters read into register 2
       machine->WriteRegister(2, readChar);
       break;
     }
     case ConsoleOutput:
-      // Console output descriptor is invalid for reading, report error (-1)
+      // Console output descriptor is not valid for reading, report error (-1)
       machine->WriteRegister(2, -1);
       break;
-
     default:  // Should be able to read any other file
       // Check if file is open
       if (currentThread->openFiles->isOpened(descriptorFile)) {
         // Read from the Unix file
-        int bytesRead =
+        int32_t bytesRead =
             read(currentThread->openFiles->getUnixHandle(descriptorFile),
                  readBuffer, size);
         // For all bytes read
-        for (int charPos = 0; charPos < bytesRead; charPos++) {
-          // Write each character into memory
+        for (int32_t charPos = 0; charPos < bytesRead; charPos++) {
+          // Write each character from the buffer into memory at the
+          // corresponding address
           machine->WriteMem(bufferAddr + charPos, 1, readBuffer[charPos]);
         }
-        // Write number of bytes read to register 2
+        // Write the number of bytes read into register 2
         machine->WriteRegister(2, bytesRead);
       } else {
         // File not open, report error (-1)
@@ -262,67 +336,79 @@ void NachOS_Read() {  // System call 6
       }
       break;
   }
-  // Don't forget to free the memory
+  // Don't forget to free the memory allocated for the buffer
   delete[] readBuffer;
+  // Increment the program counter
   NachOS_IncreasePC();
 }
 
-/*
- *  System call interface: void Close( OpenFileId )
+/**
+ * @brief NachOS Close System call
+ * This system call is used to close an open file identified by its OpenFileId.
+ *
+ * System call interface: void Close(OpenFileId)
  */
 void NachOS_Close() {  // System call 8
   // Get the OpenFileId from register 4.
   OpenFileId fileId = machine->ReadRegister(4);
-
-  // Check if file is open.
+  // Get a reference to the open files table of the current thread.
   OpenFilesTable* openFilesTable = currentThread->openFiles;
+  // Check if the file identified by the fileId is open.
   if (openFilesTable->isOpened(fileId)) {
-    // Get Unix file descriptor
-    int unixFileId = openFilesTable->getUnixHandle(fileId);
-    // Close the file.
-    int status = close(unixFileId);
+    // If open, retrieve the corresponding Unix file descriptor
+    int32_t unixFileId = openFilesTable->getUnixHandle(fileId);
+    // Attempt to close the file.
+    int32_t status = close(unixFileId);
+    // Check if the file was successfully closed.
     if (status == -1) {
-      printf("Unable to close the file with OpenFileId: %d\n", fileId);
-      // Set return value to -1 to signify error.
+      // If unsuccessful, log the failure and set return value to -1 to signify
+      // error.
+      DEBUG('c', "Unable to close the file with OpenFileId: %d\n", fileId);
       machine->WriteRegister(2, -1);
     } else {
-      // Remove file reference from open files table
+      // If successful, remove the file's reference from the open files table
+      // and set return value to 0 to signify success.
       openFilesTable->Close(fileId);
-      // Set return value to 0 to signify success.
-      machine->WriteRegister(2, 0);
     }
   } else {
-    printf("No open file with OpenFileId: %d\n", fileId);
-    // Set return value to -1 to signify error.
-    machine->WriteRegister(2, -1);
+    // If the file is not open, log the failure and set return value to -1 to
+    // signify error.
+    DEBUG('c', "Unable to close the file with OpenFileId: %d\n", fileId);
   }
 
-  // Advance program counter.
+  // Increment the program counter.
   NachOS_IncreasePC();
 }
 
-void NachOS_Fork() {  // System call 9
-  // Get the address of the function to be run by the new thread from
-  // register 4.
-  int64_t functionAddress = machine->ReadRegister(4);
-  // Create a new Thread.
-  // TODO: this ptr is not being deleted because it is not being saved
-  // anywhere
-  Thread* newThread = new Thread("Forked thread");
-  // Duplicate the address space of the current thread for the new thread.
-  newThread->space = currentThread->space;
-  newThread->openFiles = currentThread->openFiles;
-  currentThread->openFiles->addThread();
-  //   Setup the new thread's stack to run the function at functionAddress.
-  newThread->Fork(reinterpret_cast<VoidFunctionPtr>(functionAddress), 0);
-  newThread->setStatus(READY);
-  // Add the new thread to the scheduler.
-  scheduler->ReadyToRun(newThread);
-  // Advance program counter.
+/** @brief Handles the Fork system call in NachOS.
+ *
+ * The Fork system call creates a new kernel thread, shares open file table and
+ * address space (excluding stack) with the parent thread. It uses a constructor
+ * in AddrSpace class to copy the shared segments and creates a new stack for
+ * the child thread. Then, it uses kernel Fork to execute the child code,
+ * passing the user routine address as a parameter.
+ * System call interface: void Fork(void (*func)())
+ */
+void NachOS_Fork() {
+  DEBUG('f', "Entering Fork System call\n");
+  // Create a new kernel thread for user code.
+  Thread* childThread = new Thread("child to execute Fork code");
+  // Share open file table and address space (excluding stack) with the parent
+  // thread. Use AddrSpace constructor to copy shared segments and create a new
+  // stack.
+  childThread->space = new AddrSpace(currentThread->space);
+  childThread->openFiles = currentThread->openFiles;
+  childThread->openFiles->addThread();
+  // Kernel Fork to execute child code, pass the user routine address as a
+  // parameter.
+  childThread->Fork(NachosForkThread, (void*)(size_t)machine->ReadRegister(4));
+  // Adjust PC registers.
   NachOS_IncreasePC();
+
+  DEBUG('f', "Exiting Fork System call\n");
 }
 
-/*
+/**
  *  System call interface: void Yield()
  */
 void NachOS_Yield() {  // System call 10
